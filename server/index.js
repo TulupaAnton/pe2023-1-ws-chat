1698b6572b83b4f63acaa6cd65cf963f1bae3379
Anton <tulupaanton2@gmail.com>
2024-02-10 17:11:52 +0200

    Add socket.io.on

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const http = require('http');
 const app = require('./app');
+const { Server } = require('socket.io');
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', socket => {
+  console.log('Connection  :>> ');
+  socket.emit('HELLO_SELF', 'Hello on service');
+  socket.broadcast.emit('NEW_ANOTHER_SOCKET', 'New socket is added');
+  io.emit('TO_EVERYONE', 'Service will reboot at 5 minutes');
+
+  socket.on('TO_SERVER', payload => {
+    console.log('payload :>> ', payload);
+  });
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
